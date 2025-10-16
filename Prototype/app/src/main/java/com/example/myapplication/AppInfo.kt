60c1659d77125fc91194d35cf8840269eabc3833
Melissad02 <dic20006@byui.edu>
2025-10-16 14:09:39 -0600

    Updated Prototype

--- Prototype/app/src/main/java/com/example/myapplication/AppInfo.kt
+++ Prototype/app/src/main/java/com/example/myapplication/AppInfo.kt
@@ -3,5 +3,5 @@ package com.example.myapplication
 data class AppInfo(
     val name: String,
     val packageName: String,
-    var isBlocked: Boolean = false // Track blockage
+    var isBlocked: Boolean = false // Track whether app is blocked
 )
