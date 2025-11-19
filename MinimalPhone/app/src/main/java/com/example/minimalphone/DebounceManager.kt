f1daa562c5da6915ecbb5b7143f7b33d8c301c20
Lincoln Huls <168589048+lincolnhuls@users.noreply.github.com>
2025-11-19 14:27:54 -0700

    Deleted extra files

--- MinimalPhone/app/src/main/java/com/example/minimalphone/DebounceManager.kt
+++
@@ -1,7 +0,0 @@
-//package com.example.minimalphone
-//
-//class DebounceManager(
-//    private val debounceMs: Long = 300
-//){
-//    private var lastPackage: String? = null
-//}
--- MinimalPhone/app/src/main/java/com/example/minimalphone/LaunchSourceDetector.kt
+++
@@ -1,4 +0,0 @@
-//package com.example.minimalphone
-//
-//class LaunchSourceDetector {
-//}
